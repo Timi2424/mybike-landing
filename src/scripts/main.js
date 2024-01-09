d0b16907283b2d932567ccdc3a07e924c7b2894e
Timi2424 <tymonbieganski@wp.pl>
2024-01-09 16:52:39 +0100

    Add files

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const form = document.getElementById('form');
+
+form.addEventListener('clear', function handleSubmit(event) {
+  event.preventDefault();
+
+  form.reset();
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('body--with-menu');
+  } else {
+    document.body.classList.remove('body--with-menu');
+  }
+});
